ba8700a6a22e285c84ef03acbe48d4c96ef48791
Pragya <1993pragyapandey@gamil.com>
2022-12-07 01:36:14 +0530

    initial

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
 //code here 
+  const [active,setActive]=useState(true);
   return (
     <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+      <p className={active?"redColor":"blueColor"}>Newton School</p>
+      <button id='button' onClick={()=>setActive(!active)}>Change Style</button>
     </div>
   )
 }
